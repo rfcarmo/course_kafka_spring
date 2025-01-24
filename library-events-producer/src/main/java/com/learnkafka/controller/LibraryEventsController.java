f1c47f47b3ccfb13d9f03cbd5d894fadaf4b9a84
rfcarmo <rfortunatoc@gmail.com>
2025-01-24 10:10:37 +0000

    Seção 7 - Aula 28. Build LibraryEvents Producer using KafkaTemplate - Approach1- Part2

--- library-events-producer/src/main/java/com/learnkafka/controller/LibraryEventsController.java
+++ library-events-producer/src/main/java/com/learnkafka/controller/LibraryEventsController.java
@@ -1,6 +1,8 @@
 package com.learnkafka.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.learnkafka.domain.LibraryEvent;
+import com.learnkafka.producer.LibraryEventsProducer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,10 +17,18 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class LibraryEventsController {
 
+    private final LibraryEventsProducer libraryEventsProducer;
+
+    public LibraryEventsController(LibraryEventsProducer libraryEventsProducer) {
+        this.libraryEventsProducer = libraryEventsProducer;
+    }
+
     @PostMapping("/v1/libraryevent")
-    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) {
+    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException {
         log.info("libraryEvent : {} ", libraryEvent);
         // invoke the kafka producer
+        libraryEventsProducer.sendLibraryEvent(libraryEvent);
+
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
 
