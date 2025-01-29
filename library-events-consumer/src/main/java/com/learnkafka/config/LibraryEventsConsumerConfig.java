278bc69eedc5a9b7ab61405775564d5183e041eb
rfcarmo <rfortunatoc@gmail.com>
2025-01-29 11:39:23 +0000

    Seção 14 - Aula 54. Configuring the H2 In-Memory DB

--- library-events-consumer/src/main/java/com/learnkafka/config/LibraryEventsConsumerConfig.java
+++ library-events-consumer/src/main/java/com/learnkafka/config/LibraryEventsConsumerConfig.java
@@ -1,16 +1,7 @@
 package com.learnkafka.config;
 
-import org.springframework.beans.factory.ObjectProvider;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.listener.ContainerProperties;
 
 /**
  * @author rfort
@@ -18,19 +9,4 @@ import org.springframework.kafka.listener.ContainerProperties;
 @Configuration
 @EnableKafka
 public class LibraryEventsConsumerConfig {
-
-    @Autowired
-    KafkaProperties kafkaProperties;
-
-    @Bean
-    ConcurrentKafkaListenerContainerFactory<?, ?> kafkaListenerContainerFactory(
-            ConcurrentKafkaListenerContainerFactoryConfigurer configurer,
-            ObjectProvider<ConsumerFactory<Object, Object>> kafkaConsumerFactory) {
-        ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
-        configurer.configure(factory, kafkaConsumerFactory
-                .getIfAvailable(() -> new DefaultKafkaConsumerFactory<>(this.kafkaProperties.buildConsumerProperties())));
-        //factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
-        factory.setConcurrency(3); // this option is recommended if you are not running your app in a cloud environment.
-        return factory;
-    }
 }
