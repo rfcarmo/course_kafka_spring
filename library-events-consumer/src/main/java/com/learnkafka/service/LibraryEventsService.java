18e6d98bf4eedb7bcc49353bcbfd4b535385a98a
rfcarmo <rfortunatoc@gmail.com>
2025-01-30 10:14:41 +0000

    Seção 14 - Aula 57. Build Service Layer to process LibraryEvent - MODIFY Event Type

--- library-events-consumer/src/main/java/com/learnkafka/service/LibraryEventsService.java
+++ library-events-consumer/src/main/java/com/learnkafka/service/LibraryEventsService.java
@@ -9,6 +9,8 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 /**
  * @author rfort
  **/
@@ -32,10 +34,26 @@ public class LibraryEventsService {
                 save(libraryEvent);
                 break;
             case UPDATE:
+                validate(libraryEvent);
+                save(libraryEvent);
                 break;
             default:
                 log.info("Invalid Library Event type.");
         }
+    }
+
+    private void validate(LibraryEvent libraryEvent) {
+        if (libraryEvent.getLibraryEventId() == null) {
+            throw new IllegalArgumentException("Library Event ID is missing.");
+        }
+
+        Optional<LibraryEvent> libraryEventOptional = libraryEventsRepository.findById(libraryEvent.getLibraryEventId());
+
+        if (!libraryEventOptional.isPresent()) {
+            throw new IllegalArgumentException("Not a valid Library Event.");
+        }
+
+        log.info("Validation is successful for the Library Event: {}", libraryEventOptional.get());
     }
 
     private void save(LibraryEvent libraryEvent) {
